61bc58293cc28c6114c4687a005e38dc353c36fa
Luciano Ramalho <luciano@ramalho.org>
2019-06-26 16:21:35 -0500

    fixed bug in primes (2 is prime ;-)

--- spinners/python/cpu-bound/primes.py
+++ spinners/python/cpu-bound/primes.py
@@ -1,6 +1,9 @@
 import math
 
 def is_prime(n):
+    if n == 2:
+        return (n, True)
+
     if n % 2 == 0:
         return (n, False)
 
@@ -8,4 +11,5 @@ def is_prime(n):
     for i in range(3, sqrt_n + 1, 2):
         if n % i == 0:
             return (n, False)
+
     return (n, True)
--- workerpools/primes/processpool.py
+++ workerpools/primes/processpool.py
@@ -1,41 +1,21 @@
 #!/usr/bin/env python3
 
 from concurrent import futures
-import math
 import time
 import os
 
-NUMBERS = [
-    1099726899285419,
-    1570341764013157,
-    1637027521802551,
-    1880450821379411,
-    1893530391196711,
-    2447109360961063,
-    2772290760589219,
-    3033700317376073,
-    4350190374376723,
-    4350190491008389,
-    4350190491008390,
-    65956211*65956229,
-    49468369*49468387,
-    ]
+from primes import is_prime, NUMBERS
 
-def is_prime(n):
+
+def prime_test(n):
     t0 = time.perf_counter()
-    if n % 2 == 0:
-        return (False, time.perf_counter() - t0)
+    return (is_prime(n), time.perf_counter() - t0)
 
-    sqrt_n = int(math.floor(math.sqrt(n)))
-    for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return (False, time.perf_counter() - t0)
-    return (True, time.perf_counter() - t0)
 
 def main(workers):
     t0 = time.perf_counter()
     with futures.ProcessPoolExecutor(max_workers=workers) as executor:
-        future_map = {executor.submit(is_prime, n): n
+        future_map = {executor.submit(prime_test, n): n
                       for n in NUMBERS}
         for future in futures.as_completed(future_map):
             n = future_map[future]
@@ -43,7 +23,7 @@ def main(workers):
             msg = 'is' if res else 'is not'
             print(f'({dt:0.3f}s) {n:18d} {msg} prime')
     dt = time.perf_counter() - t0
-    print(f'Workers: {workers}\nTotal time: {dt}s')
+    print(f'Workers: {workers}\nTotal time: {dt:0.3f}s')
 
 
 if __name__ == '__main__':
